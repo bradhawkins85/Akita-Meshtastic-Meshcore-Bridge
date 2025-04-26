6b920a29d550fd77835ecb56fe7b0cb65a5de731
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-04-26 18:57:32 -0400

    Create __init__.py

---
+++ ammb/__init__.py
@@ -0,0 +1,12 @@
+# ammb/__init__.py
+"""
+Akita Meshtastic-Meshcore Bridge (AMMB) Package.
+
+This package contains the core modules for the AMMB application.
+"""
+
+# Make the main Bridge class easily importable
+from .bridge import Bridge
+
+# Optionally define __version__ or other package-level info
+__version__ = "0.1.0"
